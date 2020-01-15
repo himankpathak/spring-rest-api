7518237d288c188d089962e68c1966bcb19c83e0
Himank Pathak <himankpathak@gmail.com>
2020-01-15 23:32:07 +0530

    added getters and setters in Book.java

--- src/main/java/com/spingrestapi/restapidemo/Book.java
+++ src/main/java/com/spingrestapi/restapidemo/Book.java
@@ -18,6 +18,27 @@ public class Book {
     @Column(name ="author", nullable = false)
     private String author;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
 
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
