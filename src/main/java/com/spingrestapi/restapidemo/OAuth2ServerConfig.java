3183b6a4ca41d9f556fc98398f4fe45c1f82145f
Himank Pathak <himankpathak@gmail.com>
2020-01-17 14:50:34 +0530

    added AuthServerConfig extending AuthorizationServerConfigurerAdapter

--- src/main/java/com/spingrestapi/restapidemo/OAuth2ServerConfig.java
+++ src/main/java/com/spingrestapi/restapidemo/OAuth2ServerConfig.java
@@ -3,10 +3,18 @@ package com.spingrestapi.restapidemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
 public class OAuth2ServerConfig {
@@ -30,3 +38,45 @@ public class OAuth2ServerConfig {
 
         }
     }
+
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        AuthenticationManager authenticationManager;
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            String finalSecret = "{bcrypt}"+new BCryptPasswordEncoder().encode("adminpass");
+            clients.inMemory().withClient("client_1")
+                    .resourceIds(DEMO_RESOURCE_ID)
+                    .authorizedGrantTypes("client_credentials", "refresh_token")
+                    .scopes("select")
+                    .authorities("ADMIN")
+                    .secret(finalSecret)
+                    .and().withClient("client_2")
+                    .resourceIds(DEMO_RESOURCE_ID)
+                    .authorizedGrantTypes("password", "refresh_token")
+                    .scopes("select")
+                    .authorities("oauth2")
+                    .secret(finalSecret);
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+            endpoints
+                    .tokenStore(new InMemoryTokenStore())
+                    .authenticationManager(authenticationManager)
+                    .allowedTokenEndpointRequestMethods(HttpMethod.GET, HttpMethod.POST);
+        }
+
+        @Override
+        public void configure(AuthorizationServerSecurityConfigurer oauthServer) {
+            oauthServer.allowFormAuthenticationForClients();
+        }
+
+    }
+
+}
