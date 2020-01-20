ff91709aa9c5dcb4ce8976c6d23f59fa73f1c9e9
Himank Pathak <himankpathak@gmail.com>
2020-01-20 14:18:21 +0530

    updated frontend submodule

--- src/main/java/com/spingrestapi/restapidemo/OAuth2ServerConfig.java
+++ src/main/java/com/spingrestapi/restapidemo/OAuth2ServerConfig.java
@@ -87,8 +87,7 @@ public class OAuth2ServerConfig {
 
     @Configuration
     public class CorsConfig {
-        //IMPORTANT: it has to be a normal configuration class,
-        //not extending WebMvcConfigurerAdapter or other Spring Security class
+
         @Bean
         public FilterRegistrationBean customCorsFilter() {
             UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
@@ -100,9 +99,6 @@ public class OAuth2ServerConfig {
             source.registerCorsConfiguration("/**", config);
             FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
 
-            //IMPORTANT #2: I didn't stress enough the importance of this line in my original answer,
-            //but it's here where we tell Spring to load this filter at the right point in the chain
-            //(with an order of precedence higher than oauth2's filters)
             bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
             return bean;
         }
